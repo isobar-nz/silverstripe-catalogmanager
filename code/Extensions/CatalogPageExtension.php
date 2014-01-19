ad1199a137e06c10602b32566d2320f1af5e1d99
Stevie Mayhew <stevie.mayhew@littlegiant.co.nz>
2014-01-20 11:11:40 +1300

    MINOR: add parent page to main tab

--- code/Extensions/CatalogPageExtension.php
+++ code/Extensions/CatalogPageExtension.php
@@ -24,11 +24,11 @@ class CatalogPageExtension extends DataExtension
                 if ($pages->exists()) {
                     if ($pages->count() == 1) {
 
-                        $fields->push(HiddenField::create('ParentID', 'ParentID', $pages->first()->ID));
+                        $fields->addFieldToTab('Root.Main', HiddenField::create('ParentID', 'ParentID', $pages->first()->ID));
 
                     } else {
                         $parentID = $this->owner->ParentID ? : $pages->first()->ID;
-                        $fields->push(DropdownField::create('ParentID', 'Parent Page', $pages->map('ID', 'Title'), $parentID));
+                        $fields->addFieldToTab('Root.Main', DropdownField::create('ParentID', 'Parent Page', $pages->map('ID', 'Title'), $parentID));
                     }
                 } else {
                     throw new Exception('You must create a parent page of class ' . get_class($this->owner->stat('parentClass')));
